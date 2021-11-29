cd120b9260c7f93d38ee898852522e0d267a087a
Murat A <aliev.murat+git@gmail.com>
2021-11-29 22:14:32 +0300

    Executor service threads logs tracing

---
+++ src/main/kotlin/io/aligm/poc/spring/cloud/sleuth/controller/ServiceController.kt
@@ -0,0 +1,21 @@
+package io.aligm.poc.spring.cloud.sleuth.controller
+
+import io.aligm.poc.spring.cloud.sleuth.service.NewThreadTracingService
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ServiceController(val newThreadTracingService: NewThreadTracingService) {
+
+    @GetMapping("/executor-service-thread-logs-tracing")
+    fun executorServiceThreadLogsTracing(): String {
+        logger.info("Executor service thread logs tracing requested")
+        newThreadTracingService.executorServiceThreadPrintLogs()
+        return "Check application logs"
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(ServiceController::class.java)
+    }
+}
