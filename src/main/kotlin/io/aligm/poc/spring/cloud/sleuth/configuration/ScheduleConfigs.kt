fa9f211dadc28bc8b2e0d4f0ad0c4b6e37573c50
Murat A <aliev.murat+git@gmail.com>
2021-11-30 00:10:52 +0300

    Scheduled jobs thread logs tracing

---
+++ src/main/kotlin/io/aligm/poc/spring/cloud/sleuth/configuration/ScheduleConfigs.kt
@@ -0,0 +1,8 @@
+package io.aligm.poc.spring.cloud.sleuth.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@Configuration
+@EnableScheduling
+class ScheduleConfigs
