2dccda2bf703c7277d6463b5f4f997187b700407
Murat A <aliev.murat+git@gmail.com>
2021-11-30 23:44:04 +0300

    Application event's thread logs tracing

--- src/main/kotlin/io/aligm/poc/spring/cloud/sleuth/configuration/ThreadConfigs.kt
+++ src/main/kotlin/io/aligm/poc/spring/cloud/sleuth/configuration/ThreadConfigs.kt
@@ -5,6 +5,9 @@ import org.springframework.cloud.sleuth.instrument.async.LazyTraceExecutor
 import org.springframework.cloud.sleuth.instrument.async.TraceableExecutorService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.event.ApplicationEventMulticaster
+import org.springframework.context.event.SimpleApplicationEventMulticaster
+import org.springframework.context.support.AbstractApplicationContext.APPLICATION_EVENT_MULTICASTER_BEAN_NAME
 import org.springframework.scheduling.annotation.AsyncConfigurerSupport
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
@@ -32,6 +35,12 @@ class ThreadConfigs(val beanFactory: BeanFactory): AsyncConfigurerSupport() {
             initialize()
         }
         return LazyTraceExecutor(beanFactory, threadPoolTaskExecutor)
+    }
+
+    @Bean(APPLICATION_EVENT_MULTICASTER_BEAN_NAME)
+    fun applicationEventMulticaster(beanFactory: BeanFactory): ApplicationEventMulticaster {
+        return SimpleApplicationEventMulticaster(beanFactory)
+            .apply { setTaskExecutor(executor(beanFactory)) }
     }
 
 }
--- src/main/kotlin/io/aligm/poc/spring/cloud/sleuth/service/NewThreadTracingService.kt
+++ src/main/kotlin/io/aligm/poc/spring/cloud/sleuth/service/NewThreadTracingService.kt
@@ -1,6 +1,10 @@
 package io.aligm.poc.spring.cloud.sleuth.service
 
+import io.aligm.poc.spring.cloud.sleuth.event.CustomEvent
 import org.slf4j.LoggerFactory
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.ApplicationEventPublisher
+import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
 import java.util.concurrent.Executor
@@ -9,8 +13,20 @@ import java.util.concurrent.ExecutorService
 @Service
 class NewThreadTracingService(
     val executorService: ExecutorService,
-    val executor: Executor
+    val executor: Executor,
+    val applicationEventPublisher: ApplicationEventPublisher
 ) {
+
+    @EventListener(ApplicationReadyEvent::class)
+    fun onApplicationReady() {
+        logger.info("On application ready logs tracing")
+        applicationEventPublisher.publishEvent(CustomEvent())
+    }
+
+    @EventListener
+    fun onSomeEventListener(someEvent: CustomEvent) {
+        logger.info("On custom event logs tracing")
+    }
 
     fun executorServiceThreadPrintLogs() {
         executorService.execute {
